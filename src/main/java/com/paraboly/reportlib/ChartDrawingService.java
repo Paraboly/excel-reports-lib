d8c18314da507da55a00ee6a0febb89511c10d23
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-09-21 14:18:06 +0300

    making the reports/ihale usulüne göre dağılımı chart type horizontal

--- src/main/java/com/paraboly/reportlib/ChartDrawingService.java
+++ src/main/java/com/paraboly/reportlib/ChartDrawingService.java
@@ -680,12 +680,12 @@ public class ChartDrawingService {
 		XDDFChartLegend legend = XDDFchart.getOrAddLegend();
 		legend.setPosition(LegendPosition.TOP);
 
-		XDDFCategoryAxis leftAxis = XDDFchart.createCategoryAxis(AxisPosition.LEFT);
-		leftAxis.setTitle(categoryLabel);
+		XDDFCategoryAxis bottomAxis = XDDFchart.createCategoryAxis(AxisPosition.BOTTOM);
+		bottomAxis.setTitle(categoryLabel);
 
-		XDDFValueAxis bottomAxis = XDDFchart.createValueAxis(AxisPosition.BOTTOM);
+		XDDFValueAxis leftAxis = XDDFchart.createValueAxis(AxisPosition.LEFT);
 		//leftAxis.setTitle(valueLabel[0]);
-		bottomAxis.setCrossBetween(AxisCrossBetween.BETWEEN);
+		leftAxis.setCrossBetween(AxisCrossBetween.BETWEEN);
 
 		XDDFDataSource<String> cat = XDDFDataSourcesFactory.fromArray(categories);
 
@@ -694,7 +694,7 @@ public class ChartDrawingService {
 			vals.add(XDDFDataSourcesFactory.fromArray(values[i]));
 		}
 
-		XDDFChartData chartData = XDDFchart.createData(ChartTypes.BAR, leftAxis, bottomAxis);
+		XDDFChartData chartData = XDDFchart.createData(ChartTypes.BAR, bottomAxis, leftAxis);
 		chartData.setVaryColors(true);
 
 		ArrayList<XDDFChartData.Series> series = new ArrayList<>();
@@ -721,7 +721,7 @@ public class ChartDrawingService {
 		ctChart.getPlotArea().getBarChartArray(0).getDLbls().setShowCatName(ctboolean);
 
 		for(int i = 0; i < series.size(); i++){
-			XDDFchart.getCTChart().getPlotArea().getBarChartArray(0).getSerArray(i).addNewDLbls().addNewShowVal().setVal(true);
+			XDDFchart.getCTChart().getPlotArea().getBarChartArray(0).getSerArray(i).addNewDLbls().addNewShowVal().setVal(false);
 			XDDFchart.getCTChart().getPlotArea().getBarChartArray(0).getSerArray(i).getDLbls().addNewShowLeaderLines().setVal(true);
 			XDDFchart.getCTChart().getPlotArea().getBarChartArray(0).getSerArray(i).getDLbls().addNewShowSerName().setVal(false);
 			XDDFchart.getCTChart().getPlotArea().getBarChartArray(0).getSerArray(i).getDLbls().addNewShowCatName().setVal(false);
@@ -735,7 +735,7 @@ public class ChartDrawingService {
 		XDDFchart.plot(chartData);
 
 		XDDFBarChartData bar = (XDDFBarChartData) chartData;
-		bar.setBarDirection(BarDirection.BAR);
+		bar.setBarDirection(BarDirection.COL);
 		bar.setBarGrouping(BarGrouping.STANDARD);
 
 	}
